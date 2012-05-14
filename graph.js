88f8d993c3aabeb8cf8c8fc7ed5783c240910b87
Gijs Nijholt <gijs.nijholt@gmail.com>
2012-05-14 11:29:19 +0200

    …..

--- graph.js
+++ graph.js
@@ -1,1 +1,20 @@
-/* Graph code here */
+$(document).ready(function(){
+ 
+    $.ajax({
+                url: "data/metingen.json",
+                method: 'GET',
+                dataType: 'json',
+                success: onOutboundReceived
+            });
+ 
+    function onOutboundReceived(series) {
+        var length = series.length;
+        var finalData = series;
+        var options = {
+            lines: { show: true },
+            points: { show: true, hoverable:true },
+            grid: { hoverable: true, clickable: true }
+        };
+        $.plot($("#placeholder), finalData, options);
+    }
+});
